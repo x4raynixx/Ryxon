190ee15a078587f710621da0026f1f12b0481a75
Raynixx <94186653+x4raynixx@users.noreply.github.com>
2025-06-02 15:15:01 +0200

    Update install.cmd

--- install/install.cmd
+++ install/install.cmd
@@ -1,56 +1,32 @@
 @echo off
-setlocal enabledelayedexpansion
-
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" || (
-    echo Run as Administrator!
-    pause
-    exit /b 1
-)
-
-set "ARCH=x86"
-if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" set ARCH=x86_64
-if /i "%PROCESSOR_ARCHITECTURE%"=="ARM64" set ARCH=arm64
-
-set "OS=windows"
-
-set "URL_BASE=https://raw.githubusercontent.com/x4raynixx/RX-Scripting/master/install"
-
-set "FILE="
-if "%OS%"=="windows" (
-    set "FILE=rx_%ARCH%.exe"
-) else (
-    exit /b 1
-)
-
-set "INSTALL_DIR=%ProgramFiles%\RX"
-
-if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
-
-set "DEST=%INSTALL_DIR%\rx.exe"
-
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL_BASE%/%FILE%', '%DEST%')"
-
-if not exist "%DEST%" (
-    echo Download failed.
-    pause
-    exit /b 1
-)
-
-for /f "tokens=2*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path ^| findstr Path') do (
-    set "SYS_PATH=%%B"
-)
-
-echo %SYS_PATH% | find /i "%INSTALL_DIR%" >nul
+fsutil dirty query %systemdrive% >nul 2>&1
 if errorlevel 1 (
-    setx /M PATH "%SYS_PATH%;%INSTALL_DIR%"
-    if errorlevel 1 (
-        echo Failed to update PATH.
-        pause
-        exit /b 1
-    )
+    powershell -Command "Start-Process -Verb runAs -FilePath '%~f0'"
+    exit /b
 )
 
-echo RX installed to %DEST%
-echo Restart terminal or log off/on to update PATH.
+if not exist "C:\Program Files\RX" mkdir "C:\Program Files\RX"
+set RXPATH=C:\Program Files\RX
+
+set ARCH=
+for /f "tokens=2 delims==" %%a in ('wmic os get osarchitecture /value ^| find "="') do set ARCH=%%a
+
+set DOWNLOAD_URL=https://github.com/x4raynixx/RX-Scripting/raw/master/install/
+
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    set FILE=rx_x86_64.exe
+) else if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+    set FILE=rx_arm64.exe
+) else (
+    set FILE=rx_x86.exe
+)
+
+curl -L -o "%RXPATH%\rx.exe" "%DOWNLOAD_URL%%FILE%"
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%Path%;%RXPATH%" /f
+
+assoc .rx=RXFile
+ftype RXFile="%RXPATH%\rx.exe" "%%1"
+
+echo RX installed successfully
 pause
-exit /b 0
